699a66df1b487dd659d4b346d401763636b31879
PoseidonHo <poseidonhohaichi@gmail.com>
2016-01-30 13:13:19 -0500

    Implemented Inventory System

---
+++ PlayerAssets.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAssets : MonoBehaviour {
+
+	public int money;
+	public int cult;
+	public int item;
+
+	public class Assets
+	{
+
+		public Assets(int mon, int cul, int ite)
+		{
+			money = mon;
+			cult = cul;
+			item = ite;
+		}	
+
+		public Assets()
+		{
+			money = 0;
+			cult = 0;
+			item = 0;
+		}
+
+	}
+		
+	int Rally(int mon, int cul)
+	{
+		money += mon;
+		cult += cul;
+	}
+
+	int Purchase(int purchaseCost)
+	{
+		money -= purchaseCost;
+		item += 1;
+	}
+
+	int Travel(int travelCost)
+	{
+		money -= travelCost;
+	}
+
+	int Fundraise(int mon)
+	{
+		money += mon;
+	}
+
+	// Use this for initialization
+	void Awake () {
+		
+	}
+
+	void Start () {
+		Assets myAssets = new Assets();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
