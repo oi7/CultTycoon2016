a7d203d5ca7f76ebe966ee486686131c14c93cdc
PoseidonHo <poseidonhohaichi@gmail.com>
2016-01-30 23:29:05 -0500

    Implemented switchScene.cs to switch between different scenes

---
+++ Assets/switchScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class switchScene : MonoBehaviour {
+
+	public string sceneName;
+	public void switchs()
+	{
+		SceneManager.LoadScene (sceneName);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
